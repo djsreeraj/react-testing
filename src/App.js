8bd0c6f1cfb810b05328bd6bc3a1e80f93caf52b
Sreeraj Melath <sreeraj011m@gmail.com>
2024-08-06 00:04:09 +0530

    added 2 test cases and optimized

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 function App() {
   return (
     <div className="App">
-     <p>Welcome to React testing</p>
+     <h1>Welcome to React testing</h1>
+     <p>Photo by Freepik</p>
      <img title="AI generated image" src="https://fps.cdnpk.net/images/ai/tti/textures-realism.webp"/>
     </div>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -5,10 +5,14 @@ test("Test text in app", ()=> {
     render(<App />)
     //const text = screen.getByText("Welcome to React Testing") //case sensitive
     const text = screen.getByText(/Welcome to React Testing/i) //case insensitive
-
-    const title = screen.getByTitle("AI generated image")
+    const text2 = screen.getByText("Photo by Freepik")
     expect(text).toBeInTheDocument();
+    expect(text2).toBeInTheDocument()
+})
+
+test("title text in App", () => {
+    render(<App />)
+    const title = screen.getByTitle("AI generated image")
     expect(title).toBeInTheDocument()
-    
 })
 
