335fccccf627ba289b38c6b13a2e10e67070d7ce
Sreeraj Melath <sreeraj011m@gmail.com>
2024-08-05 23:57:38 +0530

    added img title test

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ function App() {
   return (
     <div className="App">
      <p>Welcome to React testing</p>
+     <img title="AI generated image" src="https://fps.cdnpk.net/images/ai/tti/textures-realism.webp"/>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -5,7 +5,10 @@ test("Test text in app", ()=> {
     render(<App />)
     //const text = screen.getByText("Welcome to React Testing") //case sensitive
     const text = screen.getByText(/Welcome to React Testing/i) //case insensitive
+
+    const title = screen.getByTitle("AI generated image")
     expect(text).toBeInTheDocument();
+    expect(title).toBeInTheDocument()
     
 })
 
